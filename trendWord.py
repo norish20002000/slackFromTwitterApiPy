759a94f7e982258f46db314479946dc41a93d0f8
首藤典宏 <norihiro-MBP@norihiro-MBP.local>
2018-09-01 00:10:02 +0900

    add url

--- trendWord.py
+++ trendWord.py
@@ -10,6 +10,8 @@ url = "https://api.twitter.com/1.1/trends/place.json"
 # url = "https://api.twitter.com/1.1/statuses/home_timeline.json"
 
 trendWord = "現在のtwitterトレンドワードは、" + "\n"
+
+# 東京
 params = {"id":"1118370"}
 
 # OAuthでget
@@ -21,19 +23,24 @@ if req.status_code == 200:
     trendDic = json.loads(req.text)
 
     # print(timeline['trends'][0])
-    # 各ツイート
+    # trend word
     for i, trendList in enumerate(trendDic):
+        # tweet_volumeでソート
         sortedVolumeList = sorted(trendList['trends'], key=lambda x: (x['tweet_volume'] is not None, x['tweet_volume']), reverse=True)
+
         for j, trend in enumerate(sortedVolumeList):
             print(trend['name'] + "  tweet_valume:" + str(trend['tweet_volume']))
+            print(trend['url'])
+            # print()
             trendWord += trend['name'] + "  tweet_valume:" + str(trend['tweet_volume']) + "\n"
 
-            if(j == 20): 
+            if(j == 10): 
                 break
 else:
     # error
     print("Error: %d" % req.status_code)
 
+# slack投稿 
 # slack = slackweb.Slack(url=AppConf.webhook)
 # slack.notify(text=trendWord)
 
